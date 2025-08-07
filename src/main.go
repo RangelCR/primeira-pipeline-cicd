dff092ebd78c568a8a770f6f96b71e1ee274b14f
Henrique Rangel <97173550+RangelCR@users.noreply.github.com>
2025-08-07 20:51:35 -0300

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo")
+	fmt.Fprintf(w, "Pantera foi uma influente banda americana de heavy metal formada em 1981 no Texas. Inicialmente com um estilo glam, ganhou destaque mundial nos anos 1990 ao adotar um som mais agressivo e pesado, conhecido como groove metal. Com Phil Anselmo nos vocais e os irmãos Dimebag Darrell (guitarra) e Vinnie Paul (bateria), o Pantera lançou álbuns marcantes como Cowboys from Hell e Vulgar Display of Power, tornando-se um dos nomes mais importantes do metal moderno.")
 }
 
 func main() {
